dbe6873f742bf1885517b4b32f355ef6e3ce8c3d
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-08-27 23:38:43 +0300

    fix width and heigh on svg

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,11 +9,12 @@ import AddRecipePage from "../../pages/AddRecipePage/AddRecipePage";
 import RecipePage from "../../pages/RecipePage/RecipePage";
 import ErrorPage from "../../pages/ErrorPage/ErrorPage";
 import Footer from "../Footer/Footer";
+import ModalAuthentication from "../ModalAuthentication/ModalAuthentication";
 
 function App() {
   return (
     <>
-      {/* <ModalAuthentication /> */}
+      <ModalAuthentication />
       {/* <AuthModal /> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
