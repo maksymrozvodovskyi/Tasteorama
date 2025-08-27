4d81ba57e33dbb091d14cd603e42374c65ef4214
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-08-27 15:13:54 +0300

    save

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -1,27 +1,33 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import styles from "./RecipeList.module.css"
+import styles from "./RecipeList.module.css";
 import { selectRecipes } from "../../redux/recipesList/selectors";
 import { fetchRecipes } from "../../redux/recipesList/operations";
 
 const RecipesList = () => {
-    const dispatch = useDispatch();
-    const recipes = useSelector(selectRecipes);
-    
-    useEffect(() => {
-        dispatch(fetchRecipes());
-    }, [dispatch])
-    console.log(recipes);
-return (
-<ul className={styles.list}>
-    {recipes.map((recipe) => (
+  const dispatch = useDispatch();
+  const recipes = useSelector(selectRecipes);
+
+  useEffect(() => {
+    dispatch(fetchRecipes());
+  }, [dispatch]);
+
+  console.log(recipes);
+
+  if (recipes.length === 0) {
+    return <p>No recipes found</p>; // тут треба викликати модалку "We’re sorry! We were not able to find a match."
+  }
+
+  return (
+    <ul className={styles.list}>
+      {recipes.map((recipe) => (
         <li key={recipe._id}>
-            {recipe.title}
-            {/*<RecipeCard recipe={recipe} />*/}
+          {recipe.title}
+          {/*<RecipeCard recipe={recipe} />*/}
         </li>
-    ))}
-</ul>
-);
-}
+      ))}
+    </ul>
+  );
+};
 
 export default RecipesList;
