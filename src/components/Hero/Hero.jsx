65ddc2f961e0bf8ff5d97c75468e1cf8a7e9473f
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-08-29 01:27:59 +0300

    fix tablet and mobile

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import css from "../../styles/container.module.css";
 export default function Hero({ onSearch }) {
   return (
     <section className={styles.hero}>
-      <div className={css.container}>
+      <div className={styles.container}>
         <div className={styles.content}>
           <h1 className={styles.title}>Plan, Cook, and Share Your Flavors</h1>
           <SearchBox onSearch={onSearch} />
