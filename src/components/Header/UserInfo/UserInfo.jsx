cc771fdc15a12bd8432d41d4c7caf080ebc89705
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-08-29 01:43:50 +0300

    save

--- src/components/Header/UserInfo/UserInfo.jsx
+++ src/components/Header/UserInfo/UserInfo.jsx
@@ -1,31 +1,34 @@
 import { useState } from "react";
 import css from "./UserInfo.module.css";
 import ConfirmLogoutModal from "../ConfirmLogoutModal/ConfirmLogoutModal";
+import { selectUser } from "../../../redux/auth/selectors";
+import { useSelector } from "react-redux";
 
 export default function UserInfo({ userName = "User", onLogout, className }) {
-    const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const user = useSelector(selectUser);
 
-    return (
-        <div className={`${css.wrapper} ${className}`}>
-            <div className={css.avatar}>{(userName?.charAt(0).toUpperCase()) || "?"}</div>
-            <span className={css.name}>{userName}</span>
-            <div className={css.divider}></div>
-            <button
-                className={css.logoutBtn}
-                onClick={() => setShowModal(true)}
-                aria-label="Logout"
-            >
-                <svg className={css.logoutIcon}>
-                    <use href="/icons.svg#icon-logout" />
-                </svg>
-            </button>
+  return (
+    <div className={`${css.wrapper} ${className}`}>
+      <div className={css.avatar}>{user.name || "?"}</div>
+      <span className={css.name}>{userName}</span>
+      <div className={css.divider}></div>
+      <button
+        className={css.logoutBtn}
+        onClick={() => setShowModal(true)}
+        aria-label="Logout"
+      >
+        <svg className={css.logoutIcon}>
+          <use href="/icons.svg#icon-logout" />
+        </svg>
+      </button>
 
-            {showModal && (
-                <ConfirmLogoutModal
-                    onClose={() => setShowModal(false)}
-                    onConfirm={onLogout}
-                />
-            )}
-        </div>
-    );
+      {showModal && (
+        <ConfirmLogoutModal
+          onClose={() => setShowModal(false)}
+          onConfirm={onLogout}
+        />
+      )}
+    </div>
+  );
 }
--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -5,7 +5,6 @@ export const selectError = (state) => state.error;
 export const selectAuthToken = (state) => state.auth.accessToken;
 export const selectAuthIsLoggedIn = (state) => Boolean(state.auth.accessToken);
 
-
 export const selectAuthIsLoading = (state) => state.auth.isLoading;
 export const selectAuthError = (state) => state.auth.error;
 // export const selectAuthUser = (state) => state.auth.user;
