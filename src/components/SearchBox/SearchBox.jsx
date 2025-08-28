619dff82e1d05620008eb8ebc84cd4b84a4c7f2a
Bohdan <toyvo7@gmail.com>
2025-08-28 18:19:44 +0200

    add hero

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import styles from "./SearchBox.module.css";
+import toast from "react-hot-toast";
+
+
+export default function SearchBox({ onSearch }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if (!searchQuery.trim()) {
+      toast.error("Введіть назву рецепту");
+      return;
+    }
+
+    onSearch(searchQuery.trim());
+
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <input
+        type="text"
+        placeholder="Search recipes"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className={styles.input}
+      />
+      <button type="submit" className={styles.button}>
+        Search
+      </button>
+    </form>
+  );
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,34 @@
-import Filters from "../../components/Filters/Filters";
-import RecipesList from "../../components/RecipeList/recipeList";
-import css from "../../styles/container.module.css";
+import { useState } from "react";
+import Hero from "../../components/Hero/Hero.jsx";
+// import RecipeList from "../components/RecipeList/RecipeList";
+// import { fetchRecipes } from "../services/api";
+// import toast from "react-hot-toast";
 
 export default function HomePage() {
+  const [recipes, setRecipes] = useState([]);
+
+  const handleSearch = async (query, filters) => {
+    //   if (!query.trim()) {
+    //     toast.error("Введіть назву рецепту!");
+    //     return;
+    //   }
+    //   try {
+    //     const data = await fetchRecipes(query, filters);
+    //     if (data.length === 0) {
+    //       setRecipes([]);
+    //       toast("Рецептів за цим запитом не знайдено!");
+    //       return;
+    //     }
+    //     setRecipes(data);
+    //   } catch (error) {
+    //     toast.error("Сталася помилка при завантаженні рецептів.");
+    //   }
+  };
+
   return (
-    <div className={css.container}>
-      <h1>HomePage</h1>
-      <Filters />
-      <RecipesList />
-    </div>
+    <>
+      <Hero onSearch={handleSearch} />
+      {/* <RecipeList recipes={recipes} /> */}
+    </>
   );
 }
