f905652363aa141d7fa5b0a04dce031df745b21e
Sergeyt2000 <sergeyt2000@gmail.com>
2025-08-29 14:58:59 +0300

    add userName

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,4 +1,5 @@
 import css from "./RegistrationForm.module.css";
+import style from "../../styles/container.module.css";
 import { initialValues, registerSchema } from "../../formSchema";
 import { registerUserThunk, loginUserThunk } from "../../redux/auth/operations";
 
@@ -9,7 +10,6 @@ import { toast } from "react-toastify";
 import { useState } from "react";
 import eyeOpenSvg from "../../assets/icons/eye.svg";
 import eyeClosedSvg from "../../assets/icons/eye-crossed.svg";
-
 
 const PasswordField = ({ field, form }) => {
   const [showPassword, setShowPassword] = useState(false);
@@ -56,8 +56,8 @@ export const RegistrationForm = () => {
   const navigate = useNavigate();
 
   const handleSubmit = async (values, { setSubmitting }) => {
-      try {
-        console.log("Submitting", values);
+    try {
+      console.log("Submitting", values);
       await dispatch(
         registerUserThunk({
           name: values.name.trim(),
@@ -69,100 +69,110 @@ export const RegistrationForm = () => {
       toast.success("Registration successful!");
 
       await dispatch(
-          loginUserThunk({ email: values.email, password: values.password })
-        ).unwrap();
+        loginUserThunk({ email: values.email, password: values.password })
+      ).unwrap();
       toast.success("Login successful!");
       navigate("/");
     } catch (error) {
-      toast.error(error.message || "Registration failed");
+      // console.error("Registration error:", error.response.data.data.message);
+      // if (error.status === 409) {
+      //   toast.error("Already in use");
+      // } else
+        toast.error(error.response.data.data.message || "Registration failed");
     } finally {
       setSubmitting(false);
     }
   };
 
   return (
-    <div className={css.formContainer}>
-      <h2 className={css.title}>Register</h2>
-      <p className={css.subtitle}>
-        Join our community of culinary enthusiasts, save your favorite recipes,
-        and share your cooking creations
-      </p>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={registerSchema}
-        onSubmit={handleSubmit}
-      >
-        {({ isSubmitting, isValid }) => (
-          <Form className={css.form}>
-            {isSubmitting && <div className={css.loader}>Loading...</div>}
-            <div className={css.fieldGroup}>
-              <label htmlFor="name" className={css.label}>
-                Enter your name
-              </label>
-              <Field
-                id="name"
-                name="name"
-                placeholder="Enter your name"
-                className={css.input}
-                autoComplete="off"
-              />
-              <ErrorMessage name="name" component="div" className={css.error} />
-            </div>
-            <div className={css.fieldGroup}>
-              <label htmlFor="email" className={css.label}>
-                Enter your email address
-              </label>
-              <Field
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Enter your email"
-                className={css.input}
-                autoComplete="off"
-              />
-              <ErrorMessage
-                name="email"
-                component="div"
-                className={css.error}
-              />
-            </div>
-            <div className={css.fieldGroup}>
-              <label htmlFor="password" className={css.label}>
-                Create a strong password
-              </label>
-              <Field name="password" component={PasswordField} />
-              <ErrorMessage
-                name="password"
-                component="div"
-                className={css.error}
-              />
-            </div>
-            <div className={css.fieldGroup}>
-              <label htmlFor="confirmPassword" className={css.label}>
-                Repeat your password
-              </label>
-              <Field name="confirmPassword" component={PasswordField} />
-              <ErrorMessage
-                name="confirmPassword"
-                component="div"
-                className={css.error}
-              />
-            </div>
-            <button
-              type="submit"
-              disabled={ isSubmitting || !isValid }
-              className={css.button}
-            >
-              Register
-            </button>
-          </Form>
-        )}
-      </Formik>
-      <div className={css.registerwrapp}>
-        <p className={css.registerwrapp_text}>Already have an account?</p>
-        <Link to="/login" className={css.registerwrapp_link}>
-          Log in
-        </Link>
+    <div className={style.container}>
+      <div className={css.formContainer}>
+        <h2 className={css.title}>Register</h2>
+        <p className={css.subtitle}>
+          Join our community of culinary enthusiasts, save your favorite
+          recipes, and share your cooking creations
+        </p>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={registerSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ isSubmitting, isValid }) => (
+            <Form className={css.form}>
+              {isSubmitting && <div className={css.loader}>Loading...</div>}
+              <div className={css.fieldGroup}>
+                <label htmlFor="name" className={css.label}>
+                  Enter your name
+                </label>
+                <Field
+                  id="name"
+                  name="name"
+                  placeholder="Enter your name"
+                  className={css.input}
+                  autoComplete="off"
+                />
+                <ErrorMessage
+                  name="name"
+                  component="div"
+                  className={css.error}
+                />
+              </div>
+              <div className={css.fieldGroup}>
+                <label htmlFor="email" className={css.label}>
+                  Enter your email address
+                </label>
+                <Field
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Enter your email"
+                  className={css.input}
+                  autoComplete="off"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className={css.error}
+                />
+              </div>
+              <div className={css.fieldGroup}>
+                <label htmlFor="password" className={css.label}>
+                  Create a strong password
+                </label>
+                <Field name="password" component={PasswordField} />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className={css.error}
+                />
+              </div>
+              <div className={css.fieldGroup}>
+                <label htmlFor="confirmPassword" className={css.label}>
+                  Repeat your password
+                </label>
+                <Field name="confirmPassword" component={PasswordField} />
+                <ErrorMessage
+                  name="confirmPassword"
+                  component="div"
+                  className={css.error}
+                />
+              </div>
+              <button
+                type="submit"
+                disabled={isSubmitting || !isValid}
+                className={css.button}
+              >
+                Register
+              </button>
+            </Form>
+          )}
+        </Formik>
+        <div className={css.registerwrapp}>
+          <p className={css.registerwrapp_text}>Already have an account?</p>
+          <Link to="/login" className={css.registerwrapp_link}>
+            Log in
+          </Link>
+        </div>
       </div>
     </div>
   );
