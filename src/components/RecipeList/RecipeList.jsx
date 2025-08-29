6085e5cb45c0de6dff1937a38668c88d09e73efe
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-08-29 16:20:52 +0300

    remove title

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./RecipeList.module.css";
-import { selectRecipes, selectTotalPages, selectCurrentPage } from "../../redux/recipesList/selectors";
+import {
+  selectRecipes,
+  selectTotalPages,
+  selectCurrentPage,
+} from "../../redux/recipesList/selectors";
 import { fetchRecipes } from "../../redux/recipesList/operations";
 import { nextPage } from "../../redux/recipesList/slice";
-import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
+import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
 import Filters from "../Filters/Filters";
 import RecipeCard from "../RecipeCard/RecipeCard";
 
@@ -19,7 +23,7 @@ const RecipesList = () => {
   const currentPage = useSelector(selectCurrentPage);
   const totalPages = useSelector(selectTotalPages);
 
-  console.log('Виклик тут', recipes);
+  console.log("Виклик тут", recipes);
 
   if (recipes.length === 0) {
     return <p>No recipes found</p>; // тут треба викликати модалку "We’re sorry! We were not able to find a match."
@@ -30,12 +34,13 @@ const RecipesList = () => {
       <ul className={styles.list}>
         {recipes.map((recipe) => (
           <li key={recipe._id}>
-            {recipe.title}
             <RecipeCard recipe={recipe} mode="default" />
           </li>
         ))}
       </ul>
-      {totalPages>0 && currentPage<totalPages &&<LoadMoreBtn nextPage={nextPage} fetchAction={fetchRecipes} />}
+      {totalPages > 0 && currentPage < totalPages && (
+        <LoadMoreBtn nextPage={nextPage} fetchAction={fetchRecipes} />
+      )}
     </>
   );
 };
