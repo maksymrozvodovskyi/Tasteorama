81b3a2266c7b1e71df16a479039667e1d6b7bd84
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-08-29 18:30:20 +0300

    qwe

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,25 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "../../pages/HomePage/HomePage";
 import LoginPage from "../../pages/LoginPage/LoginPage";
 import RegisterPage from "../../pages/RegisterPage/RegisterPage";
-import RecipeDetailsPage from "../../pages/RecipeDetailsPage/RecipeDetailsPage";
+import RecipeViewPage from "../../pages/RecipeViewPage/RecipeViewPage";
 import FavoritesPage from "../../pages/FavoritesPage/FavoritesPage";
 import MyRecipesPage from "../../pages/MyRecipesPage/MyRecipesPage";
 import AddRecipePage from "../../pages/AddRecipePage/AddRecipePage";
 import RecipePage from "../../pages/RecipePage/RecipePage";
-import ErrorPage from "../../pages/ErrorPage/ErrorPage";
 import Footer from "../Footer/Footer";
-import ModalAuthentication from "../ModalAuthentication/ModalAuthentication";
+import NotFound from "../RecipeViewPage/NotFound/NotFound";
 
 function App() {
   return (
     <>
-      <ModalAuthentication />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/recipes" element={<RecipePage />} />
-        <Route path="/recipes/:id" element={<RecipeDetailsPage />} />
+        <Route path="/recipes/:id" element={<RecipeViewPage />} />
 
         {/* приватні роути */}
         <Route path="/my-recipes" element={<MyRecipesPage />} />
@@ -28,7 +26,7 @@ function App() {
         <Route path="/favorites" element={<FavoritesPage />} />
 
         {/* ерор пейдж 404 */}
-        <Route path="*" element={<ErrorPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
