5eabb06072c6242467060135e3ff51ce3ca204ff
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-08-31 18:29:38 +0300

    save

--- src/components/ProfilePage/FavoritesList/FavoritesList.jsx
+++ src/components/ProfilePage/FavoritesList/FavoritesList.jsx
@@ -11,6 +11,8 @@ import {
 } from "../../../redux/recipesList/selectors.js";
 import { nextPage } from "../../../redux/recipesList/slice";
 import { clearFavitems } from "../../../redux/recipesList/slice";
+import { selectRecipesIsLoadingFavoriteRecipes } from "../../../redux/recipesList/selectors.js";
+import Loader from "../../Loader/Loader.jsx";
 
 const FavoritesList = () => {
   const dispatch = useDispatch();
@@ -18,18 +20,22 @@ const FavoritesList = () => {
   const total = useSelector(selectTotalFavoritesRecipes);
   const currentPage = useSelector(selectCurrentPage);
   const totalPages = useSelector(selectTotalPages);
+  const isLoadingFavoriteRecipes = useSelector(
+    selectRecipesIsLoadingFavoriteRecipes
+  );
 
   useEffect(() => {
     dispatch(clearFavitems());
     dispatch(fetchFavoriteRecipes());
   }, [dispatch]);
 
-  if (!Array.isArray(recipes) || recipes.length === 0)
-    return <h3>No favorites</h3>;
+  // if (!Array.isArray(recipes) || recipes.length === 0)
+  //   return <h3>No favorites</h3>;
 
   return (
     <div>
-      <p className={styles.recipesCount}>{total} recipes</p>
+      {total === 0 && !isLoadingFavoriteRecipes && <h3>No favorites</h3>}
+      {total !== 0 && <p className={styles.recipesCount}>{total} recipes</p>}
       <RecipeList
         recipes={recipes}
         totalPages={totalPages}
