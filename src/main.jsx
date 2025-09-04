2a6f7bfa6060a3a513ae02aa0ba41a1bb89971a2
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-09-04 17:46:04 +0300

    save

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,12 @@ import App from "./components/App/App.jsx";
 import "./styles/index.css";
 import { ToastContainer } from "react-toastify";
 import { PersistGate } from "redux-persist/integration/react";
+import Loader from "./components/Loader/Loader.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={<Loader />} persistor={persistor}>
         <BrowserRouter>
           <App />
           <ToastContainer
